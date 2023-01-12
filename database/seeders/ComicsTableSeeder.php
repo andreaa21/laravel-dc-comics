a6bc3b8ba58f7e0d49fc888337edef4f4a7cf9e8
andreaa21 <andrea.zollini@proton.me>
2023-01-12 15:30:28 +0100

    add seeder

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $array_comics = config('comics');
+        dump($array_comics);
+    }
+}
