71fa008afb0fde6c5341302f364eb5aa739c2671
andreaa21 <andrea.zollini@proton.me>
2023-01-12 18:43:10 +0100

    add table

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,4 +1,33 @@
 @extends('layout.main')
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <table class="table text-white"">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Serie</th>
+                        <th scope="col">Tipo</th>
+                        <th scope="col">Azioni</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($comics as $comic)
+                        <tr>
+                            <th scope="row">{{ $comic->id }}</th>
+                            <td>{{ $comic->title }}</td>
+                            <td>{{ $comic->series }}</td>
+                            <td>{{ $comic->type }}</td>
+                            <td>XXXX</td>
+                        </tr>
+                    @empty
+                        <p>nessun risultato trovato</p>
+                    @endforelse
+
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
