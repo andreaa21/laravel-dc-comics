2bfdd78f065aecd82df98f5966bd86f90eac56f4
andreaa21 <andrea.zollini@proton.me>
2023-01-14 16:51:06 +0100

    add validation

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -36,6 +36,37 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate(
+            [
+                'title' => 'required|max:100|min:2',
+                'image' => 'max:255|min:10',
+                'price' => 'required|max:10|min:2',
+                'series' => 'required|max:100|min:2',
+                'sale_date' => 'required|max:20|min:2',
+                'type' => 'required|max:20|min:2',
+            ],
+            [
+                'title.required' => 'il titolo è un campo obbligatorio',
+                'title.max' => 'il titolo deve avere al massimo :max caratteri',
+                'title.min' => 'il titolo deve avere minimo :min caratteri',
+                'image.max' => 'la URL dell\' immagine deve avere al massimo :max caratteri',
+                'image.min' => 'la URL dell\' immagine deve avere minimo :min caratteri',
+                'price.required' => 'il prezzo è un campo obbligatorio',
+                'price.max' => 'il prezzo deve avere al massimo :max caratteri',
+                'price.min' => 'il prezzo deve avere minimo :min caratteri',
+                'series.required' => 'la serie è un campo obbligatorio',
+                'series.max' => 'la serie deve avere al massimo :max caratteri',
+                'series.min' => 'la serie deve avere minimo :min caratteri',
+                'sale_date.required' => 'la data di vendita è un campo obbligatorio',
+                'sale_date.max' => 'la data di vendita deve avere al massimo :max caratteri',
+                'sale_date.min' => 'la data di vendita deve avere minimo :min caratteri',
+                'type.required' => 'il genere è un campo obbligatorio',
+                'type.max' => 'il genere deve avere al massimo :max caratteri',
+                'type.min' => 'il genere deve avere minimo :min caratteri'
+            ]
+        );
+
+
         $form_data = $request->all();
 
         $new_comic = new Comic();
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -4,37 +4,73 @@
     <div class="container">
         <h1>Inserisci un nuovo fumetto</h1>
 
+
         <form action="{{ route('comics.store') }}" method="POST">
             @csrf
             <div class="mb-3">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" class="form-control" name="title" id="title" placeholder="Inserire il titolo">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title"
+                    value="{{ old('title') }}" placeholder="Inserire il titolo">
+                @error('title')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Immagine</label>
-                <input type="text" class="form-control" name="image" id="image"
-                    placeholder="Inserire la URL dell'immagine">
+                <input type="text" class="form-control @error('image') is-invalid @enderror" name="image"
+                    id="image" value="{{ old('image') }}" placeholder="Inserire la URL dell'immagine">
+                @error('image')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-                <input type="text" class="form-control" name="price" id="price" placeholder="Inserire il prezzo">
+                <input type="text" class="form-control @error('price') is-invalid @enderror" name="price"
+                    id="price" value="{{ old('price') }}" placeholder="Inserire il prezzo">
+                @error('price')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Serie</label>
-                <input type="text" class="form-control" name="series" id="series" placeholder="Inserire la serie">
+                <input type="text" class="form-control @error('series') is-invalid @enderror" name="series"
+                    id="series" value="{{ old('series') }}" placeholder="Inserire la serie">
+                @error('series')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Data di Vendita</label>
-                <input type="text" class="form-control" name="sale_date" id="sale_date"
-                    placeholder="Inserire la data di vendita">
+                <input type="text" class="form-control @error('sale_date') is-invalid @enderror" name="sale_date"
+                    id="sale_date" value="{{ old('sale_date') }}" placeholder="Inserire la data di vendita">
+                @error('sale_date')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Genere</label>
-                <input type="text" class="form-control" name="type" id="type" placeholder="Inserire il genere">
+                <input type="text" class="form-control @error('type') is-invalid @enderror" name="type" id="type"
+                    value="{{ old('type') }}" placeholder="Inserire il genere">
+                @error('type')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea class="form-control" name="description" id="description" name="description" rows="3"></textarea>
+                <textarea class="form-control" name="description" id="description" name="description" rows="3">{{ old('description') }}</textarea>
             </div>
             <button type="submit" class="btn btn-primary mb-5">Invia</button>
         </form>
